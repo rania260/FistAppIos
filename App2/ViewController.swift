b1fd73e1b4da61c0185cd6cf87027c918a96479d
rania260 <raniarezgui4@gmail.com>
2024-03-06 19:41:49 +0100

    second commit

--- App2/ViewController.swift
+++ App2/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultat: UILabel!
+    @IBOutlet weak var label2: UITextField!
+    @IBOutlet weak var label1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnDiff(_ sender: Any) {
+        if let val1 = Double(label1.text),
+        let val2 = Double(label2.text)
+            {
+            let  num = val1 + val2;
+            resultat.text = "(num)";
+            
+        }
+        
+    }
+    @IBAction func btnDiv(_ sender: Any) {
+    }
+    @IBAction func btnMultiplication(_ sender: Any) {
+    }
+    @IBAction func btnAdd(_ sender: Any) {
+    }
 }
 
